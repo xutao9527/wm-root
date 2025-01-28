8c32aac8505faeb3230ba6c2093e1d1511cf4414
xutao <xutao@gmail.com>
2025-01-28 14:56:54 +0700

    12

--- wm-net/src/tcp_connection.h
+++ wm-net/src/tcp_connection.h
@@ -3,72 +3,74 @@
 #include <boost/asio.hpp>
 #include <atomic>
 #include <spdlog/spdlog.h>
+#include <thread>
 
 using boost::asio::ip::tcp;
+using boost::asio::awaitable;
+using boost::asio::use_awaitable;
 
 class tcp_connection
-    : public std::enable_shared_from_this<tcp_connection>
+	: public std::enable_shared_from_this<tcp_connection>
 {
+private:
+	tcp::socket socket_;
 public:
+	tcp_connection(tcp::socket socket)
+		: socket_(std::move(socket))
+	{
+		spdlog::info("New connection from: {}", socket_.remote_endpoint().address().to_string());
+	}
 
-  static std::shared_ptr<tcp_connection> create(boost::asio::io_context &io_context)
-  {
-    return std::shared_ptr<tcp_connection>(new tcp_connection(io_context));
-  }
-
-  tcp::socket &socket()
-  {
-    return socket_;
-  }
-
-  void start()
-  {
-    counter_ = 0;
-    do_write();
-    do_read();
-  }
+	void start()
+	{
+		co_spawn(socket_.get_executor(), [self = shared_from_this()] { return self->reader(); }, boost::asio::detached);
+		co_spawn(socket_.get_executor(), [self = shared_from_this()] { return self->writer(); }, boost::asio::detached);
+	}
 
 private:
-  tcp_connection(boost::asio::io_context &io_context)
-      : socket_(io_context), timer_(io_context)
-  {
-  }
+	awaitable<void> reader()
+	{
+		try
+		{
+			for (std::string read_msg;;)
+			{
+				std::size_t n = co_await boost::asio::async_read_until(socket_,
+					boost::asio::dynamic_buffer(read_msg, 1024), '#', use_awaitable);
+				std::string line = read_msg.substr(0, n);
+				std::stringstream thread_id_converter;
+				thread_id_converter << std::this_thread::get_id();
+				spdlog::info("read message: {}, thread id {}", line, thread_id_converter.str());
+				read_msg.erase(0, n);
+			}
+		}
+		catch (const std::exception& ex)
+		{
+			spdlog::error("reader error: {}", ex.what());
+		}
+	}
 
-  void do_write()
-  {
-    message_ = std::to_string(++counter_) + "\n";
-    boost::asio::async_write(socket_, boost::asio::buffer(message_),
-                             std::bind(&tcp_connection::handle_write, shared_from_this()));
-  }
+	awaitable<void> writer()
+	{
+		try
+		{
+			boost::asio::steady_timer timer(socket_.get_executor());
+			while (socket_.is_open())
+			{
+				std::string write_msg = "writer content#";
+				co_await boost::asio::async_write(socket_,boost::asio::buffer(write_msg), use_awaitable);
 
-  void handle_write()
-  {
-    timer_.expires_from_now(boost::posix_time::seconds(1));
-    timer_.async_wait(std::bind(&tcp_connection::do_write, shared_from_this()));
-  }
+				std::stringstream thread_id_converter;
+				thread_id_converter << std::this_thread::get_id();
+				spdlog::info("write message: {}, thread id {}", write_msg, thread_id_converter.str());
 
-  void do_read()
-  {
-    boost::asio::async_read_until(socket_, input_buffer_, '\n',
-                                  std::bind(&tcp_connection::handle_read, shared_from_this(), std::placeholders::_1));
-  }
+				timer.expires_after(std::chrono::seconds(1));
+				co_await timer.async_wait(use_awaitable);
+			}
+		}
+		catch (const std::exception& ex)
+		{
+			spdlog::error("writer error: {}", ex.what());
+		}
+	}
 
-  void handle_read(const boost::system::error_code &error)
-  {
-    if (!error) {
-      std::istream is(&input_buffer_);
-      std::string line;
-      std::getline(is, line);
-      spdlog::info("Received message: {}", line);
-      do_read();
-    } else {
-      spdlog::error("Read error: {}", error.message());
-    }
-  }
-
-  tcp::socket socket_;
-  boost::asio::deadline_timer timer_;
-  boost::asio::streambuf input_buffer_;
-  std::string message_;
-  std::atomic<int> counter_{0};
 };
--- wm-net/src/tcp_server.h
+++ wm-net/src/tcp_server.h
@@ -5,35 +5,35 @@
 #include "tcp_connection.h"
 
 using boost::asio::ip::tcp;
+using boost::asio::awaitable;
 
 
 class tcp_server
 {
 public:
-  tcp_server(boost::asio::io_context &io_context, short port)
-      : io_context_(io_context),
-        acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
-  {
-    co_spawn(io_context_, accept_connections(), boost::asio::detached);
-  }
+	tcp_server(boost::asio::io_context& io_context, short port)
+		: io_context_(io_context),
+		acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+	{
+		co_spawn(io_context_, accept_connections(), boost::asio::detached);
+	}
 
 private:
-  boost::asio::awaitable<void> accept_connections()
-  {
-    while (true)
-    {
-      try
-      {
-        auto new_connection = tcp_connection::create(io_context_);
-        co_await acceptor_.async_accept(new_connection->socket(), boost::asio::use_awaitable);
-        new_connection->start();
-      }
-      catch (const std::exception& e)
-      {
-        spdlog::error("Accept error: {}", e.what());
-      }
-    }
-  }
-  boost::asio::io_context &io_context_;
-  tcp::acceptor acceptor_;
+	awaitable<void> accept_connections()
+	{
+		while (true)
+		{
+			try
+			{
+				tcp::socket socket = co_await acceptor_.async_accept(use_awaitable);
+				std::make_shared<tcp_connection>(std::move(socket))->start();
+			}
+			catch (const std::exception& e)
+			{
+				spdlog::error("Accept error: {}", e.what());
+			}
+		}
+	}
+	boost::asio::io_context& io_context_;
+	tcp::acceptor acceptor_;
 };
